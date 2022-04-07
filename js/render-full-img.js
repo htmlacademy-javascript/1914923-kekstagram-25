5f52c91416eded914d8cd38548542cd745970732
Fitjet <andreykikin025@gmail.com>
2022-04-07 12:20:50 +0300

    Исправил пару ошибок

--- js/render-full-img.js
+++ js/render-full-img.js
@@ -7,6 +7,7 @@ const fullSizePicture = pictureWindow.querySelector('.big-picture__img img');
 const likesCount = pictureWindow.querySelector('.likes-count');
 const commentsCount = pictureWindow.querySelector('.comments-count');
 const commentsContent = pictureWindow.querySelector('.social__comments');
+const commentCopy = commentsContent.querySelector('li').cloneNode(true);
 const pictureDescription = pictureWindow.querySelector('.social__caption');
 const cancelPictureWindow = pictureWindow.querySelector('.big-picture__cancel');
 
@@ -20,7 +21,7 @@ const searchData = (item) => {
 
 const createComments = (partData) => {
   for (let i=0; i<partData.comments.length; i++) {
-    const commentItem = commentsContent.children[0].cloneNode(true);
+    const commentItem = commentCopy.cloneNode(true);
     commentItem.querySelector('.social__picture').src = partData.comments[i].avatar;
     commentItem.querySelector('.social__picture').alt = partData.comments[i].name;
     commentItem.querySelector('.social__text').textContent = partData.comments[i].message;
